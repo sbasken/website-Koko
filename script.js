4b24a593d96462aef06c431708fcf8a14e8575fa
Saki <sbasken@gmail.com>
2023-01-09 13:10:13 -0800

    add more images and script.js content removed

--- script.js
+++
@@ -1,7 +0,0 @@
-const h2 = document.createElement("h2");
-h2.textContent = "Places We Visited";
-document.querySelector("body").appendChild(h2);
-
-const h3 = document.createElement('h2');
-h3.textContent = "Food We Ate";
-document.querySelector("body").appendChild(h3);
