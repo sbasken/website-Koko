69f96581e3f0a056bca088e1c1ec9b7850246c87
Saki <sbasken@gmail.com>
2023-01-09 17:54:59 -0800

    finish website

---
+++ script.js
@@ -0,0 +1,4 @@
+const input = document.getElementByTag('img');
+input.addEventListener('click', function() {
+  alert('You should plan your next visit to a wonderful destination like Mexico!');
+});
